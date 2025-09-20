8d9ba25733940ea05a027dd8ca8081faf15d1462
miguel-merlin <mmerlin@stevens.edu>
2025-09-20 17:12:47 -0400

    feat: added env variables parsing

--- bin/blueprint_infra.ts
+++ bin/blueprint_infra.ts
@@ -1,7 +1,20 @@
 #!/usr/bin/env node
 import * as cdk from "aws-cdk-lib";
 import { BlueprintInfraStack } from "../lib/blueprint_infra-stack";
-import { props } from "../config/config";
+
+import * as dotenv from "dotenv";
+
+dotenv.config();
 
 const app = new cdk.App();
-new BlueprintInfraStack(app, "blueprint-infra-stack", props);
+new BlueprintInfraStack(app, "blueprint-infra-stack", {
+  env: {
+    account: process.env.ACCOUNT_ID,
+    region: process.env.AWS_REGION || "us-east-1",
+  },
+  senderEmail: process.env.SENDER_EMAIL || "",
+  recipientEmails: process.env.RECIPIENT_EMAILS || "",
+  domainName: process.env.DOMAIN_NAME || "",
+  subdomainName: process.env.SUBDOMAIN_NAME || "",
+  certificateArn: process.env.CERTIFICATE_ARN || "",
+});
--- lib/blueprint_infra-stack.ts
+++ lib/blueprint_infra-stack.ts
@@ -1,5 +1,7 @@
 import * as cdk from "aws-cdk-lib";
 import * as billingConstructs from "../constructs/billing-report-construct";
+import * as website from "@sitblueprint/website-construct";
+import * as pipeline from "@sitblueprint/pipeline-construct";
 import { Construct } from "constructs";
 
 export interface BlueprintInfraStackProps extends cdk.StackProps {
@@ -12,14 +14,42 @@ export interface BlueprintInfraStackProps extends cdk.StackProps {
    * Comma‐separated list of all recipients, e.g. "acct@example.com,finance@example.com"
    */
   readonly recipientEmails: string;
+
+  /**
+   * Domain name for the website, e.g. "example.com"
+   */
+  readonly domainName: string;
+
+  /**
+   * Subdomain name for the website, e.g. "app"
+   */
+  readonly subdomainName: string;
+
+  /**
+   * ARN of an existing ACM certificate in us-east-1 for the domain/subdomain
+   */
+  readonly certificateArn: string;
 }
 
 export class BlueprintInfraStack extends cdk.Stack {
+  private readonly blueprintChat: string = "blueprint-chat";
   constructor(scope: Construct, id: string, props: BlueprintInfraStackProps) {
     super(scope, id, props);
     new billingConstructs.BillingReportConstruct(this, "MonthlyBillingReport", {
       senderEmail: props.senderEmail,
       recipientEmails: props.recipientEmails,
+    });
+
+    new website.Website(this, "Blueprint-Chat-Website", {
+      bucketName: `${this.blueprintChat}-website`,
+      indexFile: "index.html",
+      errorFile: "index.html",
+      notFoundResponsePagePath: "404.html",
+      domainConfig: {
+        domainName: props.domainName,
+        subdomainName: props.subdomainName,
+        certificateArn: props.certificateArn,
+      },
     });
   }
 }
